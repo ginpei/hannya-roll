dda9dae3ba1cf3035b22a4eef85798979095ce13
ginpei <ginpei@gmail.com>
2019-03-16 05:35:15 -0700

    basic test

--- src/HannyaRoller.test.js
+++ src/HannyaRoller.test.js
@@ -1,7 +1,25 @@
 import HannyaRoller from './HannyaRoller';
+import text from './hannya-text';
 
 describe('HannyaRoller', () => {
-  it('is OK', () => {
-    expect(typeof HannyaRoller).toBe('function');
+  /** @type HannyaRoller */
+  let roller;
+
+  /** @type HTMLDivElement */
+  let el;
+
+  beforeEach(() => {
+    el = window.document.createElement('div');
+    Object.defineProperty(el, 'clientWidth', { value: 800 });
+    Object.defineProperty(el, 'clientHeight', { value: 600 });
+    roller = new HannyaRoller({ el, text });
+
+    roller.start();
+  });
+
+  it('sets up under the specified element', () => {
+    /** @type Element */
+    const elSpace = el.firstElementChild;
+    expect(elSpace.classList.contains('HannyaRoller-space')).toBeTruthy();
   });
 });
